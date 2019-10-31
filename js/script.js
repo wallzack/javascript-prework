c3a6d19dd3670e1c382efcfc22f13bbd859552b5
wallzack <vulcheek@gmail.com>
2019-10-31 22:33:26 +0100

    Add user draw

--- js/script.js
+++ js/script.js
@@ -9,7 +9,23 @@ if(randomNumber == 1){
 } else if(randomNumber == 2){
     computerMove = 'papier';
 } else if(randomNumber == 3){
-    computerMove = 'norzyce';
+    computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} if (playerInput == '2'){
+    playerMove = 'papier';
+} if (playerInput == '3')[
+    playerMove = 'nożyce'
+]
+
+printMessage('Twój ruch to: ' + playerMove);
