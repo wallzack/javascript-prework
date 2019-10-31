eba6cacd86a2f814fc6fc4850db3ba18aa8c8900
wallzack <vulcheek@gmail.com>
2019-10-31 22:46:42 +0100

    Add result info

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(playerInput == '1'){
 ]
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+      printMessage('Ja wygrywam');
+  } else if (computerMove == 'kamień' && playerMove == 'kamień'){
+      printMessage('Nikt nie wygrał');
+  } else if (computerMove == 'papier' && playerMove == 'nożyce'){
+      printMessage('Ty wygrywasz');
+  } else if (computerMove == 'papier' && playerMove == 'kamień'){
+      printMessage('Ja wygrywam');
+  } else if (computerMove == 'papier' && playerMove == 'papier'){
+      printMessage('Nikt nie wygrał');
+  } else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+      printMessage('Ty wygrywasz');
+  } else if (computerMove == 'nożyce' && playerMove == 'papier'){
+      printMessage('Ja wygrywam');
+  } else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage('Nikt nie wygrał')
+  } else if (playerMove == 'nieznany ruch'){
+      printMessage('Błąd')
+  }
